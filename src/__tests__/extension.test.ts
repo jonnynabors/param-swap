8694b8db22af8449ca4b523aec72bacc421a9342
Jonny Nabors <jonnynabors@gmail.com>
2020-01-24 15:45:26 -0500

    Throw error if more than one comma is present in text selection; Return start/end index of text selection

--- src/__tests__/extension.test.ts
+++ src/__tests__/extension.test.ts
@@ -1,27 +1,43 @@
 import {
   findAllTextBeforeLeftBracket,
   findAllTextBeforeRightBracket,
-  buildMethodSignature
+  swapParameters,
+  countCommasInText
 } from "../helpers";
+
 console.log = jest.fn();
+
 describe("Utility functions", () => {
   const validString = `(this.param1, this.param2)`;
   const invalidString = `(this.param1 this.param2)`;
   test("Return all characters from cursor to left bracket", () => {
     const result = findAllTextBeforeLeftBracket(validString, 12);
-    expect(result).toBe("this.param1,");
+
+    expect(result.leftText).toBe("this.param1,");
+    expect(result.leftBracketIndex).toBe(0);
   });
   test("Return all characters from cursor to right bracketr", () => {
     const result = findAllTextBeforeRightBracket(validString, 13);
-    expect(result).toBe(" this.param2");
+    expect(result.rightText).toBe(" this.param2");
+    expect(result.rightBracketIndex).toBe(25);
   });
   test("Return all characters from cursor between parens", () => {
-    const result = buildMethodSignature(validString, 12);
-    expect(result).toBe("this.param1, this.param2");
+    const result = swapParameters(validString, 12);
+    expect(result.text).toBe("this.param1, this.param2");
   });
   test("Throw an error if no comma is present in text selection", () => {
-    expect(() => buildMethodSignature(invalidString, 12)).toThrow(
+    expect(() => swapParameters(invalidString, 12)).toThrow(
       Error("Invalid selection, no commas between parens")
+    );
+  });
+  test("Return count of commas in text", () => {
+    const threeCommas = "Here, is, three commas, ";
+    expect(countCommasInText(threeCommas)).toBe(3);
+  });
+  test("Throw an error if more than one comma is present in text selection", () => {
+    const threeCommas = "Here, is, three commas, ";
+    expect(() => swapParameters(threeCommas, 5)).toThrow(
+      Error("Invalid selection, more than one parameter present between parens")
     );
   });
 });
--- src/helpers.ts
+++ src/helpers.ts
@@ -13,7 +13,10 @@ export function findAllTextBeforeLeftBracket(text: string, index: number) {
     currentText += currentCharacter;
     currentIndex--;
   }
-  return [...currentText].reverse().join("");
+  return {
+    leftText: [...currentText].reverse().join(""),
+    leftBracketIndex: currentIndex
+  };
 }
 
 export function findAllTextBeforeRightBracket(text: string, index: number) {
@@ -24,22 +27,48 @@ export function findAllTextBeforeRightBracket(text: string, index: number) {
   while (!bracketFound) {
     const currentCharacter = text.charAt(currentIndex);
     if (rightParens.includes(currentCharacter)) {
-      console.log("We found a left bracket");
+      console.log("We found a right bracket");
       bracketFound = true;
       break;
     }
     currentText += currentCharacter;
     currentIndex++;
   }
-  return currentText;
+  return { rightText: currentText, rightBracketIndex: currentIndex };
 }
 
-export function buildMethodSignature(text: string, index: number) {
-  const allTextBetweenParens =
-    findAllTextBeforeLeftBracket(text, index) +
-    findAllTextBeforeRightBracket(text, index + 1);
-  if (!allTextBetweenParens.includes(",")) {
+export function swapParameters(text: string, index: number) {
+  const { leftText, leftBracketIndex } = findAllTextBeforeLeftBracket(
+    text,
+    index
+  );
+  const { rightText, rightBracketIndex } = findAllTextBeforeRightBracket(
+    text,
+    index + 1
+  );
+  const allTextBetweenParens = leftText + rightText;
+
+  if (countCommasInText(allTextBetweenParens) === 0) {
     throw new Error("Invalid selection, no commas between parens");
+  } else if (countCommasInText(allTextBetweenParens) > 1) {
+    throw new Error(
+      "Invalid selection, more than one parameter present between parens"
+    );
   }
-  return allTextBetweenParens;
+
+  return {
+    text: allTextBetweenParens,
+    leftBracketIndex,
+    rightBracketIndex
+  };
+}
+
+export function countCommasInText(text: string) {
+  let commaCount = 0;
+  [...text].forEach(character => {
+    if (character === ",") {
+      commaCount++;
+    }
+  });
+  return commaCount;
 }
